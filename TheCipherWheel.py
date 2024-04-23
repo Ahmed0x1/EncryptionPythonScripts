fad6e47811ecf4056e00404d407af31a2893bc3b
Ahmed0x1 <167270706+Ahmed0x1@users.noreply.github.com>
2024-04-23 10:14:35 -0400

    Update TheCipherWheel.py

--- TheCipherWheel.py
+++ TheCipherWheel.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 OutC = [['A', 'B', 'C', 'D', 'E'],# 0 -> 5
                #0    1    2    3    4
                ['F', 'G', 'H', 'I', 'J'],# 1 -> 5
@@ -17,18 +19,19 @@ InnC = [['A', 'B', 'C', 'D', 'E'],# 0 -> 5
                ['P', 'Q', 'R', 'S', 'T'],# 3 ->5
                ['U', 'V', 'W', 'X', 'Y']# 4 -> 5
                ]# 5 -> 1
+
 EndPoint = "Z"                
 PointE = OutC[0][0]
 SavSecuPoint = [PointE]
-#		ENCRYPTION
+#		Prepare Functions For Encryption & Decryption functions
 def indexSearchSecKey(K , arr):
 	if isinstance(K , str):
 		for i in range(len(arr)):
 		    for j in range(len(arr[i])):
 		    	if arr[i][j] == K:
 				    return i , j
-				    
-	elif isinstance(K , int):
+						    
+	if isinstance(K , int):
 		limit = 0
 		for i in range(len(arr)):
 			for j in range(len(arr[i])):
@@ -70,15 +73,32 @@ def ModifyArr(K , savPoint , arr):
 	for i in range(len(StanderdArr)):
 		ModifyArrT.append(StanderdArr[i])
 	return ModifyArrT
-	
+
 def StanderdArray(Arr):
 	res = []
 	for i in range(len(Arr)):
-		   for j in range(len(Arr[i])):
-				  res.append(Arr[i][j])
+		for j in range(len(Arr[i])):
+			res.append(Arr[i][j])
 	res.append('Z')
 	return res
 
+def ArithSearchIndex(str1):
+	lest = []
+	lestNumberOfIndex = []
+	stand = StanderdArray(InnC)
+	stand.append('Z')
+	for i in range(len(str1)):
+		lest.append(str1[i])
+
+	for le in range(len(lest)):
+		for searchIndex in range(len(stand)):
+			if lest[le] == stand[searchIndex]:
+				lestNumberOfIndex.append(searchIndex)
+			else:
+				continue
+	return lestNumberOfIndex
+
+#		ENCRYPTION
 def SCStr(str1 , K):
 	lest = []
 	for i in range(len(str1)):
@@ -97,6 +117,34 @@ def SCStr(str1 , K):
 				
 	return Encrypt
 
+def SCWithArithemtic(str1Arr , K):
+	lest = []
+	for i in range(len(str1Arr)):
+		if str1Arr[i] == " ":
+			continue
+		else:
+			lest.append(str1Arr[i])
+	Encrypt = ""
+	stand = StanderdArray(OutC)
+	lestNum = ArithSearchIndex(lest)
+	op = 0
+	sav = []
+	for i in range(len(lestNum)):
+		op = K + lestNum[i]
+		if op > 25:
+			if op == 26:
+				sav.append(op - 26)
+			elif op > 26:
+				sav.append(op - 26)
+		else:
+			sav.append(op)
+
+	for j in range(len(sav)):
+		for c in range(len(stand)):
+			if sav[j] == c:
+				Encrypt += stand[c]
+
+	return Encrypt
 #		DECRYPTION
 def SDCStr(str1 , K):
 	lest = []
@@ -116,14 +164,41 @@ def SDCStr(str1 , K):
 					Dencrypt += stand[st]
 				
 	return Dencrypt
-#		STRUCTER FLOW IPUML
+
+def SDCWithArithemtic(str1Arr , K):#in Fix
+	lest = []
+	for i in range(len(str1Arr)):
+		if str1Arr[i] == " ":
+			continue
+		else:
+			lest.append(str1Arr[i])
+		
+	Dencrypt = ""
+	stand = StanderdArray(OutC)
+	lestNum = ArithSearchIndex(lest)
+	op = 0
+	sav = []
+	for i in range(len(lestNum)):
+		op = lestNum[i] - K
+		if op >= 0:
+			sav.append(stand[op])
+		if op < 0:
+			op = op + 26
+			sav.append(stand[op])
+			
+	for j in range(len(sav)):
+		Dencrypt += sav[j]
+	return Dencrypt
+#		STRUCTER FLOW 
 def theMainList():
-	print("[1] Encrypt Message")
-	print("[2] Decrypt Message")
-	print("[3] Exit")
+	print("[1] Encrypt Message (The Cipher Wheel)")
+	print("[2] Decrypt Message (The Cipher Wheel)")
+	print("[3] Encrypt Message (The Cipher Wheel With Arithmetic)")
+	print("[4] Decrypt Message (The Cipher Wheel With Arithmetic)")
+	print("[5] Exit")
 	try:
 		cho = int(input("choice: "))
-		if isinstance(cho , int) or cho <= 3 and cho >= 1:
+		if isinstance(cho , int) or cho <= 4 and cho >= 1:
 			return cho
 	except:
 		print("Error in your choice please, Don't enter string letters, Enter Integer number only..\n")
@@ -137,10 +212,36 @@ def ifErr():
 		elif con == 2:
 			decryptFlow()
 		elif con == 3:
+			encryptWithArithemticFlow()
+		elif con == 4:
+			decryptWithArithemticFlow()
+		elif con == 5:
 			exit()
 		else:
 			print("Error in your choice please,s Choice another one between 1 to 3.\n")
 			ifErr()
+
+def decryptWithArithemticFlow():
+	try:
+		Key = int(input("Enter Security Key(0 to 25): "))
+		if isinstance(Key , int) and Key >= 0 and Key <= 25 :
+			writeM = str(input("Enter your message for encryption: "))
+			print("\nYour Message Has Encrypted:-> " , SDCWithArithemtic(writeM.upper() , Key) , "\n")
+	except:
+		print("Please Enter Integer Number Only For Your Security Key.\n")
+		ifErr()
+	print("------------------------------------------------------------------------------\n")
+
+def encryptWithArithemticFlow():
+	try:
+		Key = int(input("Enter Security Key(0 to 25): "))
+		if isinstance(Key , int) and Key >= 0 and Key <= 25 :
+			writeM = str(input("Enter your message for encryption: "))
+			print("\nYour Message Has Encrypted:-> " , SCWithArithemtic(writeM.upper() , Key) , "\n")
+	except:
+		print("Please Enter Integer Number Only For Your Security Key.\n")
+		ifErr()
+	print("------------------------------------------------------------------------------\n")
 
 def decryptFlow():
 	try:
@@ -169,6 +270,7 @@ def encryptFlow():
 	print("------------------------------------------------------------------------------\n")
 
 def main():
+	print("-----------------------------------------------------------------------------------")
 	print("-----------------------------THE CIPHER WHEEL PROGRAME-----------------------------")
 	print("Letters: " , StanderdArray(OutC) , " :Your Wheel")
 	print("-----------------------------------------------------------------------------------")
@@ -182,6 +284,10 @@ def main():
 			elif con == 2:
 				decryptFlow()
 			elif con == 3:
+				encryptWithArithemticFlow()
+			elif con == 4:
+				decryptWithArithemticFlow()
+			elif con == 5:
 				exit()
 			
 		
